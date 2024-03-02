057cf22759edb10a09e50ac6e767f6b9e3e908a5
Javier Lobato <davidlobato99@gmail.com>
2024-03-02 00:10:32 -0500

    Add to main import flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:moviepedia/config/router/app_router.dart';
 import 'package:moviepedia/config/theme/app_theme.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+
+  await dotenv.load(fileName: '.env');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,8 +19,6 @@ class MyApp extends StatelessWidget {
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      
-
     );
   }
 }
